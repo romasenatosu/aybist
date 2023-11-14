ab5075da6f14c8c672631ec2efbff5df7543976a
Muhammet Ali <inserveofgod@gmail.com>
2023-11-14 21:42:33 +0100

    more error pages added

--- pages/errors/403.php
+++ pages/errors/403.php
@@ -4,8 +4,8 @@
             <div class="col-lg-4">
                 <div class="text-center">
                     <img src="assets/images/backgrounds/errorimg.svg" alt="" class="img-fluid" width="500">
-                    <h1 class="fw-semibold mb-7 fs-9">HATA!!!</h1>
-                    <h4 class="fw-semibold mb-7">Server Hatası.</h4>
+                        <h1 class="fw-semibold mb-7 fs-9">HATA!!!</h1>
+                    <h4 class="fw-semibold mb-7">Bu sayfaya erişiminiz kapalı.</h4>
                     <a class="btn btn-primary" href="?page=home" role="button">Ana Sayfaya Dön</a>
                 </div>
             </div>
--- pages/errors/404.php
+++ pages/errors/404.php
@@ -3,8 +3,8 @@
         <div class="row justify-content-center w-100">
             <div class="col-lg-4">
                 <div class="text-center">
-                    <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/backgrounds/errorimg.svg" alt="" class="img-fluid" width="500">
-                    <h1 class="fw-semibold mb-7 fs-9">HATA!!!</h1>
+                    <img src="assets/images/backgrounds/errorimg.svg" alt="" class="img-fluid" width="500">
+                        <h1 class="fw-semibold mb-7 fs-9">HATA!!!</h1>
                     <h4 class="fw-semibold mb-7">İstediğiniz sayfaya ulaşılamıyor.</h4>
                     <a class="btn btn-primary" href="?page=home" role="button">Ana Sayfaya Dön</a>
                 </div>
